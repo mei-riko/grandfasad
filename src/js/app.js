4a888eda0361216d6fccf1757b970e3750f2fc6d
mei-riko <dobrovolska.marina@gmail.com>
2019-07-24 01:57:12 +0300

    index page

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,21 @@
 import $ from 'jquery'
 $(document).ready(() =>{
-
+    $("a.scroll").click(function() {
+        $("html, body").animate({
+           scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+           duration: 500,
+           easing: "swing"
+        });
+        return false;
+    });
+    
+    // Index Slider
+    $('.slider .slider__image').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        fade: true,
+    });
 });
